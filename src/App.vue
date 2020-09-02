dae83a3d96041f53beba079edb48c6bb925d3740
Sebastian Ospina <sebastianospinacontacto@gmail.com>
2020-09-02 09:30:00 -0500

    Intento descargar app

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,26 @@ export default {
     CapturarEmail,
   },
   mounted() {
+
+    window.addEventListener('beforeinstallprompt', function(e) {
+		// beforeinstallprompt Event fired
+
+		// e.userChoice will return a Promise.
+		// For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
+		e.userChoice.then(function(choiceResult) {
+
+			console.log(choiceResult.outcome);
+
+			if(choiceResult.outcome == 'dismissed') {
+				console.log('User cancelled home screen install');
+			}
+			else {
+				console.log('User added to home screen');
+      }
+      })
+      });
+      
+
     if (localStorage.getItem("ingresoMail") == "true") {
       this.dialog = false;
     } else {
